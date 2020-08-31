43da46c1ec00aa52e8d9673327a3f796abb45880
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-08-31 23:12:10 +0000

    Create apacherange.py

---
+++ modules/apacherange.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from requests import get
+from threading import Thread
+from queue import Queue
+from core import good,cyan,rest,bmagenta,Byellow,yellow,magenta
+def RNG(host):
+    r = get(host,headers={'Range':'bytes=0-,5-0,5-1,5-2,5-3,5-4,5-5'},verify=False,allow_redirects=False)
+    if 'Content-Range: bytes'.encode('utf-8') in r.content:
+        print(f'''
+{yellow}[ {cyan}RANGE HEADER{rest} {yellow}]
+{good} Found >> {host}
+{"-"*13}|
+''')
+q = Queue()
+def threader():
+    while True:
+        item = q.get()
+        RNG(item)
+        q.task_done()
+def run(opts):
+    for i in range(opts['threads']):
+        p1 = Thread(target=threader)
+        p1.daemon = True
+        p1.start()
+    for url in opts['url']:
+        q.put(url)
+    q.join()
