a4c3dbed6627d4dbd95bc3254682bed5d3b4c871
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2021-01-18 23:20:47 +0200

    Update template.py

--- modules/template.py
+++ modules/template.py
@@ -26,10 +26,6 @@ class Import:
         while True:
             item = q.get()
             item[1]['url'] = item[0]
-<<<<<<< HEAD
-#            print(item[1])
-=======
->>>>>>> c6175aed85e1dca6c5a909faa90f298ea2ee79d2
             c.main(item[1])
             q.task_done()
     def run(opts):
@@ -39,8 +35,4 @@ class Import:
             p1.start()
         for url in opts['url']:
             q.put(handeropts(url,opts))
-<<<<<<< HEAD
         q.join()
-=======
-        q.join()
->>>>>>> c6175aed85e1dca6c5a909faa90f298ea2ee79d2
