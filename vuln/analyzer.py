f2680efcb6c57104c349cb3beca77e4a69fa5146
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2021-01-24 21:52:32 +0200

    fix post/put parameter

--- vuln/analyzer.py
+++ vuln/analyzer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from libs import NewRequest as nq 
+from libs import post_data
 from libs import insertAfter as inf
 from urllib.parse import urlparse
 from random import randint
@@ -18,7 +19,7 @@ class methods:
             return 0
     def Post(url,data=None):
         try:
-            if nq.Post(url.split('?')[0],urlparse(url).query).status_code != 405:
+            if nq.Post(url.split('?')[0],post_data(urlparse(url).query)).status_code != 405:
                 return 1
             else:
                 return 0
@@ -26,7 +27,7 @@ class methods:
             return 0
     def Put(url,data=None):
         try:
-            if nq.Put(url.split('?')[0],urlparse(url).query).status_code != 405:
+            if nq.Put(url.split('?')[0],post_data(urlparse(url).query)).status_code != 405:
                 return 1
             else:
                 return 0
@@ -47,7 +48,7 @@ class refxss:
             return 0
     def Post(url):
         try:
-            r = nq.Post(url.split('?')[0],inf(urlparse(url).query,'=','scantrrr'))
+            r = nq.Post(url.split('?')[0],post_data(inf(urlparse(url).query,'=','scantrrr')))
             if r.content.decode().lower().find('scantrrr') != -1:
                 return 1
             else:
@@ -56,7 +57,7 @@ class refxss:
             return 0
     def Put(url):
         try:
-            r = nq.Put(url.split('?')[0],inf(urlparse(url).query,'=','scantrrr'))
+            r = nq.Put(url.split('?')[0],post_data(inf(urlparse(url).query,'=','scantrrr')))
             if r.content.decode().lower().find('scantrrr') != -1:
                 return 1
             else:
@@ -79,7 +80,7 @@ class refcrlf:
             return 0
     def Post(url):
         try:
-            r = nq.Post(url.split('?')[0],inf(urlparse(url).query,'=','scantrrr'))
+            r = nq.Post(url.split('?')[0],post_data(inf(urlparse(url).query,'=','scantrrr')))
             for header,value in r.headers.items():
                 if 'scantrrr' in header or 'scantrrr' in value:
                     return 1
@@ -89,7 +90,7 @@ class refcrlf:
             return 0
     def Put(url):
         try:
-            r = nq.Put(url.split('?')[0],inf(urlparse(url).query,'=','scantrrr'))
+            r = nq.Put(url.split('?')[0],post_data(inf(urlparse(url).query,'=','scantrrr')))
             for header,value in r.headers.items():
                 if 'scantrrr' in header or 'scantrrr' in value:
                     return 1
