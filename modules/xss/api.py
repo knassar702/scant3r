bf22443f2546ed29f2f2fa4c26b3817bdc52d224
Khaled Nassar <knassar702@gmail.com>
2021-07-16 00:34:38 +0200

    fix xss module error with api

--- modules/xss/api.py
+++ modules/xss/api.py
@@ -2,7 +2,7 @@ from .xss import Scan
 from urllib.parse import urlparse
 
 def main(opts,http):
-    scanner = Scan(opts,r)
+    scanner = Scan(opts,http)
     if urlparse(opts['url']).query:
         return scanner.start(url=opts['url'],opts['methods'])
     return
