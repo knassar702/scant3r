0a3ace61566e7e38746a9f1dc748015611af913c
Marius Vinaschi <marius.vinaschi@telindus.lu>
2021-07-20 11:03:52 +0200

    Update Lorsrf Structure

--- modules/python/lorsrf/lorsrf.py
+++ modules/python/lorsrf/lorsrf.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 __author__ = 'Khaled Nassar'
 __email__ = 'knassar702@gmail.com'
-__version__ = '0.6#Beta'
-from core.libs import post_data,urlencoder
+__version__ = '0.8#Beta'
+
 from threading import Thread
 from queue import Queue
 from urllib.parse import urlparse # url parsing
@@ -17,50 +17,48 @@ parameters_in_one_request = 10
 
 # ?ex1=http://google.com&ex2=http://google.com
 
+class Scan:
+    def __init__(self, opts, http):
+        self.opts = opts 
+        self.http = http
+    
+    def scan(self): 
+        for _ in range(int(self.opts['threads'])):
+            p1 = Thread(target=self.threader)
+            p1.daemon = True
+            p1.start()
+        for url in self.org():
+            q.put(url)
+        q.join()
 
-def threader(host,http,methods):
-    while True:
-        item = q.get()
-        lor(item,host,http,methods)
-        q.task_done()
+    def threader(self):
+        while True: 
+            url = q.get()
+            self.lor(url)
+            q.task_done()
 
-def org(url,host):
-    l = len(ssrf_parameters())
-    newurl = url
-    allu = []
-    for par in ssrf_parameters():
-        pay = f'{host}/{par}'
-        if newurl != url:
-            if len(urlparse(newurl).query) > 0:
-                newurl += f'&{par}={pay}'
-            else:
-                newurl += f'?{par}={pay}'
+    def lor(self, url: str):
+        for method in self.opts['methods']:
+            if method == 'GET':
+                r = self.http.send(method, url)
+            else: 
+                r = self.http.send(method, url.split('?')[0], body=urlparse(url).query)
+
+    def check_url(self, url: str, par: str, pay: str) -> str:
+        if len(urlparse(url).query) > 0:
+            return f'&{par}={pay}'
         else:
-            if len(urlparse(url).query) > 0:
-                newurl += f'&{par}={pay}'
-            else:
-                newurl += f'?{par}={pay}'
-        if len(urlparse(newurl).query.split('=')) == parameters_in_one_request + 1:
-            allu.append(newurl)
-            newurl = url
-    return allu
-
-def lor(url,host,http,methods=['GET','POST']):
-    for method in methods:
-        if method == 'GET':
-            r = http.send(method,url)
-        else:
-            r = http.send(method,url.split('?')[0],body=urlparse(url).query)
-
-def start(opts,http):
-    if opts['host']:
-        pass
-    else:
-        return
-    for _ in range(int(opts['threads'])):
-        p1 = Thread(target=threader,args=(opts['host'],http,opts['methods']))
-        p1.daemon = True
-        p1.start()
-    for url in org(opts['url'],opts['host']):
-        q.put(url)
-    q.join()
+            return f'?{par}={pay}'
+    
+    def org(self):
+        l = len(ssrf_parameters())
+        newurl = self.opts['url']
+        allu = []
+        for par in ssrf_parameters():
+            pay = f"{self.opts['host']}/{par}"
+            newurl = self.check_url(newurl, par, pay)
+            if len(urlparse(newurl).query.split('=')) == parameters_in_one_request + 1:
+                allu.append(newurl)
+                newurl = self.opts['url']
+        return allu 
+        
