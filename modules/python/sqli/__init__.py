93660d07a2d566e485072f49a39b25648f7e6e6d
Marius Vinaschi <marius.vinaschi@telindus.lu>
2021-07-20 10:52:37 +0200

    Update Structure SQLI

--- modules/python/sqli/__init__.py
+++ modules/python/sqli/__init__.py
@@ -1,30 +1,8 @@
-from core.libs import insert_to_params_urls, alert_bug
-from wordlists import sqli_payloads,sql_err
-from urllib.parse import urlparse
-from re import findall
-from queue import Queue
-from threading import Thread
+from .sqli import Scan
 
-
-def start(op,http):
-    for method in op['methods']:
-        for payload in sqli_payloads:
-            payload = payload.rstrip()
-            n = insert_to_params_urls(op['url'],payload)
-            for url in n:
-                if method == 'GET':
-                    r = http.send(method,url)
-                else:
-                    r = http.send(method,op['url'].split('?')[0],body=urlparse(url).query)
-                for v in sql_err:
-                    v = v.rstrip()
-                    if len(v.rstrip()) >= 1:
-                        hmm = findall(v,r.text)
-                        for i in hmm:
-                            if i:
-                                alert_bug('SQL injection',r,payload=urlparse(url).query,match=v)
-    return {}
-def main(opts,r):
-    c = start(opts,r)
+def main(opts, r):
+    s = Scan(opts, r)    
+    c = s.scan()
+    # C is always an empty dict
     if c:
-        return c
+        return c 
