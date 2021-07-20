5047edde0de6ac0ce43c8c9b0d18e39d8d6d26be
Marius Vinaschi <marius.vinaschi@telindus.lu>
2021-07-20 10:47:27 +0200

    Update Structure SSTI

--- modules/python/ssti/__init__.py
+++ modules/python/ssti/__init__.py
@@ -1,30 +1,5 @@
-from urllib.parse import urlparse
-from wordlists import ssti_payloads as ssti
-from core.libs import alert_bug,insert_to_params_urls
-
-
-class Scan:
-    def __init__(self,opts,r):
-        self.opts = opts
-        self.http = r
-    def scan(self,url,methods=['GET','POST']):
-        for method in methods:
-            for payload,match in ssti().items():
-                nurl = insert_to_params_urls(url,payload)
-                for n in nurl:
-                    if method == 'GET':
-                        r = self.http.send(method,n)
-                    else:
-                        r = self.http.send(method,url.split('?')[0],body=urlparse(n).query)
-                    if r != 0: # 0 = Connection error:
-                        if match in r.text:
-                            return {
-                                    'http':r,
-                                    'target':n,
-                                    'match':match,
-                                    'payload':payload,
-                                    }
-
+from .ssti import Scan
+from core.libs import alert_bug
 
 def main(opts,r):
     s = Scan(opts,r)
