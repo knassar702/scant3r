bb111410b067e85035234a7b05b57588b9f52765
Marius Vinaschi <marius.vinaschi@telindus.lu>
2021-07-20 11:10:44 +0200

    Update Structure Finder

--- modules/python/finder/__init__.py
+++ modules/python/finder/__init__.py
@@ -1,53 +1,10 @@
-from yaml import safe_load
-from re import findall
-from core.libs import alert_bug,remove_dups,dump_request,dump_response
-from urllib.parse import urlparse
+from .finder import Scan
+from core.libs import alert_bug
 
-def start(op,http):
-    f = safe_load(open('modules/finder/find.yaml','r'))
-    found = dict()
-    for method in op['methods']:
-        url = op['url']
-        if method == 'GET':
-            r = http.send(method,url)
-        else:
-            r = http.send(method,url.split('?')[0],body=urlparse(url).query)
-        for name,data in f.items():
-            hm = {}
-            for i in data:
-                for v,vv in i.items():
-                    hm[v] = vv
-            if 'part' in hm.keys():
-                if hm['part'] == 'request':
-                    part = dump_request(r)
-                else:
-                    part = dump_response(r)
-            else:
-                part = dump_response(r)
-            if hm['regex']:
-                m = findall(hm['text'],part)
-                for ff in m:
-                    if ff:
-                        found[name] = {
-                                'find':name,
-                                'http':r,
-                                'match':hm["text"],
-                                'regex':m
-                                }
-            else:
-                if hm['text'] in part:
-                    found[name] = {
-                                'find':name,
-                                'http':r,
-                                'method':method,
-                                'match':hm["text"],
-                                'regex':False
-                                }
-    return found
-def main(opts,r,api=False):
-    v = start(opts,r)
-    if v:
-        for i,c in v.items():
+def main(opts, r, api=False):
+    result = Scan(opts, r).scan()
+    if result:
+        for i,c in result.items():
             alert_bug('FINDER',**c)
         if api:
-            return v
+            return result
