80f14123d3ec8627c1839e41e97bfed752e40aa8
Marius Vinaschi <mariusvinaschi@gmail.com>
2021-07-21 13:52:27 +0200

    clean xss_param

--- modules/python/xss_param/xss_param.py
+++ modules/python/xss_param/xss_param.py
@@ -15,13 +15,11 @@ class XssParam(Scan):
                 r = self.http.send(method,u)
             else:
                 r = self.http.send(method,u.split('?')[0],body=urlparse(u).query)
-            if r != 0:
-                if txt in r.text:
-                    ref.append(txt)
+            if r != 0 and txt in r.text:
+                ref.append(txt)
         return ref
     
     def start(self):
-        http = self.http
         for method in self.opts['methods']:
             v = self.reflect(self.opts['url'],method=method)
             if len(v) > 0:
@@ -29,10 +27,9 @@ class XssParam(Scan):
                     payload = payload.rstrip()
                     for nurl in insert_to_params_name(self.opts['url'],urlencoder(payload)):
                         if method == 'GET':
-                            r = http.send(method,nurl)
+                            r = self.http.send(method,nurl)
                         else:
-                            r = http.send(method,self.opts['url'].split('?')[0],body=urlparse(nurl).query)
-                        if r != 0:
-                            if payload in r.text:
-                                print(f'[XSS Parameter Name: {method}] :> {nurl}\n')
-                                return {method:nurl}
+                            r = self.http.send(method,self.opts['url'].split('?')[0],body=urlparse(nurl).query)
+                        if r != 0 and payload in r.text:
+                            print(f'[XSS Parameter Name: {method}] :> {nurl}\n')
+                            return {method:nurl}
