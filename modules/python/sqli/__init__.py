84f9fb0f536c03d7c90588a06f2770a91bcdc707
Marius Vinaschi <mariusvinaschi@gmail.com>
2021-07-21 16:58:20 +0200

    Remove c in SQLI issue 31

--- modules/python/sqli/__init__.py
+++ modules/python/sqli/__init__.py
@@ -2,7 +2,4 @@ from .sqli import Sqli
 from core.libs import Http
 
 def main(opts: dict, http: Http):
-    c = Sqli(opts, http).start()
-    # C is always an empty dict
-    if c:
-        return c 
+    Sqli(opts, http).start()
