76f718c1a8935b521097eace256ba0de45943951
Khaled Nassar <knassar702@gmail.com>
2021-07-26 22:55:24 +0200

    add limit for match output lenght

--- modules/python/cve/cve.py
+++ modules/python/cve/cve.py
@@ -18,15 +18,15 @@ class Cve(Scan):
     def start(self) -> dict:
         results = []
         # load python CVE modules
-        for module in glob(f'{self.path}cve/list/python/CVE*.py'):
+        for module in glob(f'{self.path}cve/list/python/*.py'):
             module = import_module(self.transform_path_to_module_import(module))
             with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
                 results.append(executor.submit(module.main(self.opts['url'], self.http)))
 
         # load YAML CVE templates
-        for template in glob(f'{self.path}cve/list/templates/CVE*.yaml'):
+        for template in glob(f'{self.path}cve/list/templates/*.yaml'):
             template = self.open_yaml_file(template, False)
-            log.debug('parsing CVE template')
+            log.debug('parsing Scanning template')
             name = template['id']
             methods = template['request']['method']
             paths = template['request']['paths']
@@ -55,9 +55,14 @@ class Cve(Scan):
                     request = self.http.send(method, url, allow_redirects=follow_redirects, headers=headers)
                     if type(request) != list:
                         if match[1]['regex'] == True:
-                            finder = re.findall(match[0]['word'], request.text)
+                            finder = re.findall(match[0]['word'], request.text,re.DOTALL)
                             if finder:
-                                alert_bug(name, request, match=finder, regex=True)
+                                if len(''.join(finder)) > 700:
+                                    for i in finder:
+                                        if len(i) > 700:
+                                            finder.remove(i)
+                                            finder.append(i[0:700] + '   <<<<< ETC >>>>>')
+                                alert_bug(name, request,match=match[0]['word'],regex=True, found=finder)
                         else:
                             if match[0]['word'] in request.text:
                                 alert_bug(name,request,match=match[0]['word'],regex=False)
