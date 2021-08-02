a1960446a8813a8f653fe278e1feb8c9f8ee214f
Khaled Nassar <knassar702@gmail.com>
2021-08-02 22:31:59 +0200

    fix nim module example

---
+++ core/libs/nim/__init__.py
@@ -0,0 +1,2 @@
+import nimporter
+from .url import *
--- core/libs/nim/url.nim
+++ core/libs/nim/url.nim
@@ -1,25 +1,10 @@
-import uri, strformat
-
+import nimpy,uri
 
 type Parser = object
 
-proc printUri(self:Parser, url: string) =
-  let res = parseUri(url)
-  if res.scheme != "":
-    echo &"\t  Scheme: {res.scheme}"
-  if res.hostname != "":
-    echo &"\tHostname: {res.hostname}"
-  if res.username != "":
-    echo &"\tUsername: {res.username}"
-  if res.password != "":
-    echo &"\tPassword: {res.password}"
-  if res.path != "":
-    echo &"\t    Path: {res.path}"
-  if res.query != "":
-    echo &"\t   Query: {res.query}"
-  if res.port != "":
-    echo &"\t    Port: {res.port}"
-
+proc printUri(self:Parser, url: string): string {.exportpy.} =
+  let res = "YES", parseUri(url).hostname
+  return res
 
 # const test = Parser()
 # test.printUri("http://knassar702.github.io/?test=1")
