dd956b88f2d4a80a733e0a953383f1b0ae93a229
Khaled Nassar <knassar702@gmail.com>
2021-11-29 07:29:30 +0200

    Add Multi Threads to firebase module]

--- modules/python/firebase/firebase.py
+++ modules/python/firebase/firebase.py
@@ -4,8 +4,8 @@ from modules import Scan
 from core.libs import alert_bug
 from wordlists import TLD
 from logging import getLogger
-import tldextract
 from core.libs import Http
+import tldextract,concurrent.futures
 
 log = getLogger('scant3r')
 
@@ -18,23 +18,25 @@ class Firebase(Scan):
         host = tldextract.extract(self.opts['url']).domain
         log.debug(f"Trying to Find open Firebase database for {host}")
         all_hosts = [host]
-        
+        tasks = [] 
         for tld in TLD():
             all_hosts.append(host + tld.rstrip())
-            
-        for target_host in all_hosts:
-            firebase = FIREBASE_URL % target_host
-            read_request = self.http.send('GET',firebase + '/.json')
-            if type(read_request) == list:
-                return
-            log.debug(f'Check for Read permission -> {firebase}')
-            if read_request.status_code == 200:
-                alert_bug('Firebase',read_request,permission="Read enabled",status=200,content_length=len(read_request.text))
-            log.debug(f'Check for Write permission -> {firebase}')
-            write_request = self.http.send('PUT',firebase + '/firebase/security.json',body={"msg":"scant3r"},org=False)
-            if type(write_request) == list:
-                return
-            if write_request.status_code == 200:
-                alert_bug('Firebase',read_request,permission="Write enabled",status=200,content_length=len(write_request.text))
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            for target_host in all_hosts:
+                tasks.append(executor.submit(self.scan,target_host)) 
+    def scan(self,target_host):
+        firebase = FIREBASE_URL % target_host
+        read_request = self.http.send('GET',firebase + '/.json')
+        if type(read_request) == list:
+            return
+        log.debug(f'Check for Read permission -> {firebase}')
+        if read_request.status_code == 200:
+            alert_bug('Firebase',read_request,permission="Read enabled",status=200,content_length=len(read_request.text))
+        log.debug(f'Check for Write permission -> {firebase}')
+        write_request = self.http.send('PUT',firebase + '/firebase/security.json',body={"msg":"scant3r"},org=False)
+        if type(write_request) == list:
+            return
+        if write_request.status_code == 200:
+            alert_bug('Firebase',read_request,permission="Write enabled",status=200,content_length=len(write_request.text))
         
         return {} 
