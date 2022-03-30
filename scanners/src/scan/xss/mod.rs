45419167077ba67c81d17b675dfdb18cdec30932
knassar702 <knassar702@gmail.com>
2022-03-30 23:53:06 +0200

    Remove unused varable

--- scanners/src/scan/xss/mod.rs
+++ scanners/src/scan/xss/mod.rs
@@ -77,52 +77,43 @@ impl XssUrlParamsValue for Xss<'_> {
     ) -> HashMap<url::Url, String> {
         let mut _found: HashMap<url::Url, String> = HashMap::new();
         for param in self.value_reflected().await {
-            for payload in &payloads {
-                if payload.len() == 0 {
+            let mut req = self.request.clone();
+            req.url = self.injector.set_urlvalue(&param, "hackerman");
+            let res = match req.send().await {
+                Ok(resp) => resp,
+                Err(e) => {
+                    error!("{}", e);
                     continue;
                 }
-                let payload = payload
-                    .replace("JS_FUNC", "alert")
-                    .replace("JS_VALUE", "scanttrr");
-                let mut req = self.request.clone();
-                req.url = self.injector.set_urlvalue(&param, "hackerman");
-                let res = match req.send().await {
-                    Ok(resp) => resp,
-                    Err(e) => {
-                        error!("{}", e);
-                        continue;
-                    }
+            };
+            for x in parse(&res.body.as_str(), "hackerman".to_string()).iter() {
+                /*
+                 * Check if the payload is in the html and analyze it for chosen tags
+                 * */
+                let Payloads = XssPayloads {
+                    js_cmd: vec!["pp".to_string()],
+                    js_value: vec!["1".to_string()],
+                    html_tags: vec!["<img src=x JS_FUNC(JS_VALUE)>".to_string()],
                 };
-                for x in parse(&res.body.as_str(), "hackerman".to_string()).iter() {
-                    /*
-                     * Check if the payload is in the html and analyze it for chosen tags
-                     * */
-                    req.url = self.injector.set_urlvalue(&param, &payload);
-                    let Payloads = XssPayloads {
-                        js_cmd: vec!["pp".to_string()],
-                        js_value: vec!["1".to_string()],
-                        html_tags: vec!["<img src=x JS_FUNC(JS_VALUE)>".to_string()],
+                let PayloadGenerator =
+                    PayloadGen::new(&res.body.as_str(), x, "hackerman", &Payloads);
+                for pay in PayloadGenerator.analyze().iter() {
+                    req.url = self.injector.set_urlvalue(&param, &pay.payload);
+                    match req.send().await {
+                        Ok(resp) => {
+                            let d = html_search(resp.body.as_str(), &pay.search);
+                            if d.len() > 0 {
+                                _prog.println(format!(
+                                    "FOUND DOM XSS {:?} | {:?} | {:?}",
+                                    x, pay.payload, d
+                                ));
+                                break;
+                            }
+                        }
+                        Err(_e) => {
+                            continue;
+                        }
                     };
-                    let PayloadGenerator =
-                        PayloadGen::new(&res.body.as_str(), x, "hackerman", &Payloads);
-                    for pay in PayloadGenerator.analyze().iter() {
-                        req.url = self.injector.set_urlvalue(&param, &pay.payload);
-                        match req.send().await {
-                            Ok(resp) => {
-                                let d = html_search(resp.body.as_str(), &pay.search);
-                                if d.len() > 0 {
-                                    _prog.println(format!(
-                                        "FOUND DOM XSS {:?} | {:?} | {:?}",
-                                        x, pay.payload, d
-                                    ));
-                                    break;
-                                }
-                            }
-                            Err(_e) => {
-                                continue;
-                            }
-                        };
-                    }
                 }
             }
         }
