d323e8d4ea8ce00bc791c8a8bde33b261df2445b
Khaled Nassar <knassar702@gmail.com>
2022-05-27 23:35:12 +0200

    Add debugging msgs

--- modules/python/ssti/__init__.py
+++ modules/python/ssti/__init__.py
@@ -15,7 +15,7 @@ class Main(Scan):
         for method in self.opts["methods"]:
             for payload in SSTI_PAYLOADS:
                 new_url = insert_to_params_urls(self.opts["url"], payload)
-                self.log.debug("SSTI: GENERATE A NEW URL: {new_url}")
+                self.log.debug(f"SSTI: GENERATE A NEW URL: {new_url}")
                 response = self.send_request(method, new_url, self.opts["url"])
                 if response.__class__.__name__ == "Response":
                     self.log.debug("SSTI: MATCHING  WITH scan10tr")
