80cc09c3328817ef2074258a90101f9354ad054b
Khaled Nassar <knassar702@gmail.com>
2022-05-27 23:35:43 +0200

    Add Type

--- modules/python/scan.py
+++ modules/python/scan.py
@@ -1,7 +1,7 @@
 from base64 import b64encode
 from logging import getLogger
 from secrets import token_bytes
-from typing import Union
+from typing import Dict, Union, Any
 from urllib.parse import parse_qsl, urlparse
 
 from requests.models import Response
@@ -15,7 +15,7 @@ PATH_PYTHON_MODULE = f"{base_dir}/modules/python/"
 
 class Scan:
     def __init__(
-        self, opts: dict, http: httpSender, tag: str, path: str = PATH_PYTHON_MODULE
+        self, opts: Dict[str,Any], http: httpSender, tag: str, path: str = PATH_PYTHON_MODULE
     ):
         self.opts = opts
         self.http = http
