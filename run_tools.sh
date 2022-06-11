3200b337efaa34718e559ec45e49ccc34b2eb0b3
Khaled Nassar <knassar702@gmail.com>
2022-06-11 22:17:40 +0200

    Remove run_tools

--- run_tools.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip3 install .
-printf 'http://localhost:5000/?u=hello\nhttp://example.com\nhttp://localhost:5000/dat/?ff=1' | ~/.local/bin/scant3r -m all -o data.json # --proxy http://localhost:8080i/
