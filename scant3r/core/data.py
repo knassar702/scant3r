d386aee87ee139d4709ed86077e0f2a4b7934944
Khaled Nassar <knassar702@localhost.localdomain>
2022-06-11 12:46:01 +0200

    Tuple instead of List

--- scant3r/core/data.py
+++ scant3r/core/data.py
@@ -6,22 +6,21 @@ from rich.console import Console
 base_dir = Path(__file__).resolve().parent.parent
 
 # interact.sh public servers
-INTERACT_SERVERS = [
+INTERACT_SERVERS = (
     "interact.sh",
     "oast.pro",
     "oast.live",
     "oast.site",
     "oast.online",
     "oast.me",
-]
-
+)
 LOGGING_FORMAT = "%(name)-12s: %(levelname)-8s %(message)s"
 LOGGING_FILE = os.path.join(Path.home(), ".scant3r.log")
 CLEAR_LOGGING_FILE = True
 
 
 # ENABLED Modules ( after using `-m all` option)
-ENABLED_MODS = ["ssti", "firebase", "req_callback"]
+ENABLED_MODS = ("ssti", "firebase", "req_callback")
 
 # CLI
 LOGO = open(f"{base_dir}/conf/logo.txt", "r").read()
@@ -29,19 +28,18 @@ console = Console()
 FIREBASE_URL = "https://%s.firebaseio.com"
 
 # PAYLOADS
-SQLI_PAYLOADS = Path(f"{base_dir}/db/txt/sqli.txt").read_text().splitlines()
-SQL_ERRORS = Path(f"{base_dir}/db/txt/sqli_errors.txt").read_text().splitlines()
+SQLI_PAYLOADS = tuple(Path(f"{base_dir}/db/txt/sqli.txt").read_text().splitlines())
+SQL_ERRORS = tuple(Path(f"{base_dir}/db/txt/sqli_errors.txt").read_text().splitlines())
 
-PATH_TRAVERSAL = Path(f"{base_dir}/db/txt/traversal.txt").read_text().splitlines()
-TLD = Path(f"{base_dir}/db/txt/tld.txt").read_text().splitlines()
+PATH_TRAVERSAL = tuple(Path(f"{base_dir}/db/txt/traversal.txt").read_text().splitlines())
+TLD = tuple(Path(f"{base_dir}/db/txt/tld.txt").read_text().splitlines())
 
-RCE_PAYLOADS_PWD = Path(f"{base_dir}/db/txt/pwd.txt").read_text().splitlines()
-RCE_PAYLOADS_PASSWD = Path(f"{base_dir}/db/txt/passwd.txt").read_text().splitlines()
+RCE_PAYLOADS_PWD = tuple(Path(f"{base_dir}/db/txt/pwd.txt").read_text().splitlines())
+RCE_PAYLOADS_PASSWD = tuple(Path(f"{base_dir}/db/txt/passwd.txt").read_text().splitlines())
 
-SSTI = Path(f"{base_dir}/db/txt/ssti.txt").read_text().splitlines()
-SSRF_PARAMS = Path(f"{base_dir}/db/txt/ssrf_parameters.txt").read_text().splitlines()
-XSS_JS_FUNC = Path(f"{base_dir}/db/txt/js_func.txt").read_text().splitlines()
-XSS_JS_VALUE = Path(f"{base_dir}/db/txt/js_value.txt").read_text().splitlines()
-XSS_TAGS = Path(f"{base_dir}/db/txt/xss.txt").read_text().splitlines()
-XSS_ATTR = Path(f"{base_dir}/db/txt/xss_attr.txt").read_text().splitlines()
-# BLIND_XSS_PAYLOADS = Path(f"{base_dir}/db/txt/bxss.txt", "r").read_text().splitlines()
+SSTI = tuple(Path(f"{base_dir}/db/txt/ssti.txt").read_text().splitlines())
+SSRF_PARAMS = tuple(Path(f"{base_dir}/db/txt/ssrf_parameters.txt").read_text().splitlines())
+XSS_JS_FUNC = tuple(Path(f"{base_dir}/db/txt/js_func.txt").read_text().splitlines())
+XSS_JS_VALUE = tuple(Path(f"{base_dir}/db/txt/js_value.txt").read_text().splitlines())
+XSS_TAGS = tuple(Path(f"{base_dir}/db/txt/xss.txt").read_text().splitlines())
+XSS_ATTR = tuple(Path(f"{base_dir}/db/txt/xss_attr.txt").read_text().splitlines())
